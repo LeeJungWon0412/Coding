6402d6e6f9d9aada38071c69cf206945c4157b09
LeeJungWon0412 <ljw171417@naver.com>
2025-07-01 11:15:35 +0900

    feat: 백준 10813번 공 바꾸기 문제 풀이

--- PYTHON-Study/baekjoon/4단계/10813.py
+++ PYTHON-Study/baekjoon/4단계/10813.py
@@ -4,7 +4,7 @@ arr = []
 for i in range(n):
     arr.append(i + 1)
 
-for a in range(m): 
+for a in range(m):  
     i, j = map(int, input().split())
     k = arr[i - 1]
     arr[i - 1] = arr[j - 1]
